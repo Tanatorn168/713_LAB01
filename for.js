b6f4a76df3667d1bfc2c37c3b1c1cbc562edeadd
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-08 00:11:55 +0700

    task7.5

--- for.js
+++ for.js
@@ -7,3 +7,13 @@ let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
 for (let i = 0; i < color.length; i++) {
     console.log(color[i])
 }
+
+let Name = ['Jonathan', 'Robert', 'Sarah', 'เจน', 'Kevin']
+for (let i = 0; i < Name.length; i++) {
+    if (Name[i].length > 6){
+        console.log(`สวัสดีคุณ ${Name[i]}`)
+    }
+    else {
+        console.log(`Hello ${Name[i]}`)
+    }
+}
