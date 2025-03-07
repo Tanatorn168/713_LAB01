5b0ef1c7164195e2ad584415d7dc0e58ebec1cbd
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-08 00:20:34 +0700

    task9

--- function.js
+++ function.js
@@ -1,11 +1,5 @@
-function calculateTotal(scores) {
-    let total = 0;
-    for (let i = 0; i < scores.length; i++) {
-        total += scores[i]; 
-    }
-    return total; 
+const greet = function greet(name){
+    return 'Hello ' + name
 }
 
-let studentScores = [10, 20, 30];
-let totalScore = calculateTotal(studentScores);
-console.log("ผลรวมคะแนนทั้งหมด:", totalScore);
+console.log(greet('John'))
