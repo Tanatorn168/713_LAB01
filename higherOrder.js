3da0dfc6a6202ac838c050c3b4f9a016d94d1c45
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-08 22:57:01 +0700

    task12.3

--- higherOrder.js
+++ higherOrder.js
@@ -1,6 +1,15 @@
-const n = [1, 2, 3, 4, 5]
-//const square = n.map((num) => num * num)
-//console.log(square)
+const n = [1, 2, 3, 4, 5];
+//const square = n.map((num) => num * num);
+//console.log(square);
 
 const even = n.filter((num) => num % 2 === 0);
 console.log(even);
+
+const processNumbers = (numbers) => {
+    return numbers
+        .filter(num => num % 3 ===0)
+        .map(num => num**3)
+}
+
+const result = processNumbers(n);
+console.log(result);
