d4f02fffc63a4cf1731792b321c363a0f6f4692d
Tanatorn168 <tanatorn_ch@cmu.ac.th>
2025-03-08 22:46:38 +0700

    task11.4

--- json.js
+++ json.js
@@ -1,8 +1,24 @@
-const object = {
-    name: "John",
-    age: 30,
-    city: "New York"
+//const object = {
+    //name: "John",
+    //age: 30,
+    //city: "New York"
+//}
+//console.log(object.name)
+//const jsonStr = JSON.stringify(object)
+//console.log(jsonStr)
+
+const checkStudentStatus = (student) => {
+    if (student.gpa > 2) {
+        return JSON.stringify(student)
+    } else {
+        return `คุณ ${student.name} พ้นสภาพ`
+    }
 }
-console.log(object.name)
-const jsonStr = JSON.stringify(object);
-console.log(jsonStr)
+
+const student1 = {
+    name: "Jonathan",
+    lastname: "Jackson",
+    age: 25,
+    gpa: 1
+}
+console.log(checkStudentStatus(student1))
